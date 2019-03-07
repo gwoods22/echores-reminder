a6764320a0c1521321d8481be4bb4d7269bad8fb
Graeme Woods <graemewoods202@gmail.com>
2019-03-07 17:26:36 -0500

    remove extra files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# README
-[<img src="https://code.stdlib.com/button/button.svg" height="32">](https://code.stdlib.com/)
+## Emerson Chores Reminder
+### The best chore reminder around
 
-## The best chore reminder around
+
 Scheduled Stdlib script gets phone numbers and current roles from Firebase then texts the numbers with Message Bird
--- functions/__main__.js
+++ functions/__main__.js
@@ -1,16 +1,23 @@
-import db from '../firebaseconfig';
-const firebase = require('firebase/app');
-
 /**
 * A simple "hello world" function
 */
 module.exports = async () => {
-  b.collection('users').onSnapshot(querySnapshot => {
-    var newArray = [];
-    querySnapshot.forEach(userDoc => {
-      newArray.push(userDoc.data());
-      this.setState({people:newArray});
-    });
-  }
-  return newArray;
+  // const firebase = require('firebase/app');
+  // require("firebase/firestore");
+  // firebase.initializeApp({
+  //   apiKey: 'AIzaSyAlWvWD1ISY8c2_QOCZ7H_Spj9GlpiolP4',
+  //   authDomain: "emersonchores.firebaseapp.com",
+  //   projectId: "emersonchores"
+  // });
+  // const db = firebase.firestore();
+  // const settings = { timestampsInSnapshots: true};
+  // db.settings(settings);
+  // db.collection('users').onSnapshot(querySnapshot => {
+  //   var newArray = [];
+  //   querySnapshot.forEach(userDoc => {
+  //     newArray.push(userDoc.data());
+  //     this.setState({people:newArray});
+  //   });
+  // }
+  return "newArray";
 };
